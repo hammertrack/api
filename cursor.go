08a1dc6277d18d2877c2b973bcf43fa0a064ae3c
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2022-04-03 12:46:30 +0200

    add todo

--- cursor.go
+++ cursor.go
@@ -13,6 +13,8 @@ func (c Cursor) Encode() string {
 }
 
 func cursorFromString(s string) Cursor {
+	// TODO - maybe we could minimize memory allocations reusing buffers from
+	// other cursors by using a pool of buffers
 	dbuf := make([]byte, len(s))
 	n, err := base64.URLEncoding.Decode(dbuf, utils.StrToByte(s))
 	if err != nil {
